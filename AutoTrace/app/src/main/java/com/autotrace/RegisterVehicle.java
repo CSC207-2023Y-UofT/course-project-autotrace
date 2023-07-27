29f6a0d18df619cfe29fe6340ab097a92480b2b3
Haris Malik <112772076+harismalik-1@users.noreply.github.com>
2023-07-27 13:20:18 -0400

    <feat>: Registering-New-Vehicle Screen Created

---
+++ AutoTrace/app/src/main/java/com/autotrace/RegisterVehicle.java
@@ -0,0 +1,16 @@
+package com.autotrace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class RegisterVehicle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_vehicle);
+        EditText vehicleInfo
+    }
+}
