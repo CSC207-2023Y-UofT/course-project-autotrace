ba52d072b4e786c81d2eaade1b3f784382870667
Diler Zaza <95944688+l0minous@users.noreply.github.com>
2023-07-30 12:46:04 -0400

    Added Log In Screen Java File

---
+++ app/src/main/java/com/course_project_autotrace/LogInScreen.java
@@ -0,0 +1,14 @@
+package com.course_project_autotrace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogInScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_screen);
+    }
+}
