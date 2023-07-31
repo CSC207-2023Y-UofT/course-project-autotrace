4e3f094618add19e82d42053e1e23bc6f5de8187
ji24077 <80298064+Hanji11@users.noreply.github.com>
2023-07-31 19:12:52 -0400

    <Feat>: Created a Sing Up Page + Integrated Firebase Database to create a user + Note: Loginji goes to MainActivity that needs to be changed later to it's respective class

--- app/src/main/java/com/course_project_autotrace/Loginji.java
+++ app/src/main/java/com/course_project_autotrace/Loginji.java
@@ -1,13 +1,19 @@
 package com.course_project_autotrace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth; // Import FirebaseAuth
 import com.google.firebase.database.DatabaseReference;
 
@@ -19,6 +25,8 @@ public class Loginji extends AppCompatActivity {
     //private Button mBtnsignup; 밑에꺼대신씀.
 
     TextView textViewSignUp = findViewById(R.id.textView7);//diler put sigup button as text.
+   //TextView textViewForgotpwd = findViewById(R.id.textView..)
+    private Button mBtnContinueLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +38,35 @@ public class Loginji extends AppCompatActivity {
         // @override
         //public void onClick(View v){
         //String  strEmail
+
+        mBtnContinueLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public Void onClick(View v){
+                String strEmail = mEtEmail.getText().toString();
+                String strPwd = mEtPwd.getText().toString();
+                mFirebaseAuth.signInWithEmailAndPassword(strEmail,strPwd).addOnCompleteListener(Loginji.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent intent = new Intent(Loginji.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(Loginji.this, "login failed", Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
+            }
+        });
         startActivity(new Intent(Loginji.this, Signup.class)); //사인업 스크린으로 이동.
+        mBtnContinueLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Loginji.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
