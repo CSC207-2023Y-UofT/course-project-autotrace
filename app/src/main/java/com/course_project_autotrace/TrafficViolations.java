f84678f00279c8d54e455db52f3e12e2a6e26ab1
l0minous <zazadiler7@gmail.com>
2023-08-03 04:49:10 -0400

    Created XML layout files fore Traffic Violations and CarInfo Screens

---
+++ app/src/main/java/com/course_project_autotrace/TrafficViolations.java
@@ -0,0 +1,14 @@
+package com.course_project_autotrace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrafficViolations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traffic_violations);
+    }
+}
