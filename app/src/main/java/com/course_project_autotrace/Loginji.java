ddc64a84b14c03abbf43c55f721c99ae2d9195af
l0minous <zazadiler7@gmail.com>
2023-08-03 00:20:45 -0400

    Fixed Issues with Splash Screen, Empty Text Fields, Screen Ordering

--- app/src/main/java/com/course_project_autotrace/Loginji.java
+++ app/src/main/java/com/course_project_autotrace/Loginji.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+//import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,69 +20,62 @@ import com.google.firebase.auth.FirebaseAuth; // Import FirebaseAuth
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import org.w3c.dom.Text;
+//import org.w3c.dom.Text;
 
 public class Loginji extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth; // for firebase autheniation
-    private DatabaseReference mDatabaseRef; //for realtime database.
     private EditText mEtEmail; //for edit text.
     private EditText mEtPwd;
-    private ImageButton backButton;
     //private Button mBtnsignup; //since we using imagebutton on frontend , we use above.
-
-    private TextView textViewSignUp;//diler put sigup button as text.
-   //TextView textViewForgotpwd = findViewById(R.id.textView..)
-    private Button mBtnContinueLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginji);
         mFirebaseAuth = FirebaseAuth.getInstance();
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        //for realtime database.
+        FirebaseDatabase.getInstance().getReference();
         mEtEmail = findViewById(R.id.editEmail);
-        textViewSignUp = findViewById(R.id.textView7);
+        //diler put sigup button as text.
+        TextView textViewSignUp = findViewById(R.id.textView7);
         mEtPwd = findViewById(R.id.editPassword);
-        mBtnContinueLogin = findViewById(R.id.continueButton2);
-        backButton = findViewById(R.id.BackBtn); // Initialize the backButton using findViewById
+        //TextView textViewForgotpwd = findViewById(R.id.textView..)
+        Button mBtnContinueLogin = findViewById(R.id.continueButton2);
+        ImageButton backButton = findViewById(R.id.BackBtn); // Initialize the backButton using findViewById
 
-        mBtnContinueLogin.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                String strEmail = mEtEmail.getText().toString();
-                String strPwd = mEtPwd.getText().toString();
-                mFirebaseAuth.signInWithEmailAndPassword(strEmail,strPwd).addOnCompleteListener(Loginji.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Intent intent = new Intent(Loginji.this,HomeScreen.class);
-                            startActivity(intent);
-                            finish();
-                        }else{
-                            Toast.makeText(Loginji.this, "Failed login", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
-        });
-        startActivity(new Intent(Loginji.this, Signup.class));
+        mBtnContinueLogin.setOnClickListener(v -> {
+            String strEmail = mEtEmail.getText().toString();
+            String strPwd = mEtPwd.getText().toString();
 
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Your existing code for backButton click
-                Intent intent = new Intent(Loginji.this, Signup.class);
-                startActivity(intent);
+            // Check if the email or password fields are empty
+            if(strEmail.isEmpty() || strPwd.isEmpty()){
+                Toast.makeText(Loginji.this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+                return;  // stop further execution if fields are empty
             }
+
+            mFirebaseAuth.signInWithEmailAndPassword(strEmail,strPwd).addOnCompleteListener(Loginji.this, task -> {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(Loginji.this,HomeScreen.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(Loginji.this, "Failed login", Toast.LENGTH_SHORT).show();
+                }
+            });
         });
 
-        textViewSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Your existing code for textViewSignUp click
-                Intent intent = new Intent(Loginji.this, Signup.class);
-                startActivity(intent);
-            }
+//        startActivity(new Intent(Loginji.this, Signup.class));
+
+        backButton.setOnClickListener(v -> {
+            // Your existing code for backButton click
+            Intent intent = new Intent(Loginji.this, Signup.class);
+            startActivity(intent);
+        });
+
+        textViewSignUp.setOnClickListener(v -> {
+            // Your existing code for textViewSignUp click
+            Intent intent = new Intent(Loginji.this, Signup.class);
+            startActivity(intent);
         });
     }
 
