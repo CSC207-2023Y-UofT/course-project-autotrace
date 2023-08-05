15905e85bafa23e51541527468ef05023585a574
Haris Malik <112772076+harismalik-1@users.noreply.github.com>
2023-08-04 23:18:08 -0400

    Changes the failure toast message

--- app/src/main/java/com/course_project_autotrace/RegisterNewVehicle.java
+++ app/src/main/java/com/course_project_autotrace/RegisterNewVehicle.java
@@ -16,9 +16,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.Objects;
-
 
 public class RegisterNewVehicle extends AppCompatActivity {
     private DatabaseReference referenceToUsers, referenceToCars;
@@ -58,7 +56,7 @@ public class RegisterNewVehicle extends AppCompatActivity {
                         referenceToUsers.child(userID).child(Objects.requireNonNull(car.getKey())).setValue(car.getValue()).addOnSuccessListener(aVoid -> {
                             Toast.makeText(RegisterNewVehicle.this, "Car added successfully", Toast.LENGTH_LONG).show();
                             // Here you can also navigate to another activity if needed
-                        }).addOnFailureListener(e -> Toast.makeText(RegisterNewVehicle.this, "Failed to add car", Toast.LENGTH_LONG).show());
+                        }).addOnFailureListener(e -> Toast.makeText(RegisterNewVehicle.this, "Failed to add car in the database", Toast.LENGTH_LONG).show());
                     }
                     else{
                         Toast.makeText(RegisterNewVehicle.this, "Car does not exist in the database", Toast.LENGTH_LONG).show();
