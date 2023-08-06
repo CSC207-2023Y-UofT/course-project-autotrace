662d04385edeb407681cebf33e4206b983d97ae6
Haris Malik <112772076+harismalik-1@users.noreply.github.com>
2023-08-06 18:56:05 -0400

    Fixed minor warnings

--- app/src/main/java/com/course_project_autotrace/RegisterNewVehicle/RegisterNewVehiclePresenter.java
+++ app/src/main/java/com/course_project_autotrace/RegisterNewVehicle/RegisterNewVehiclePresenter.java
@@ -3,10 +3,12 @@ package com.course_project_autotrace.RegisterNewVehicle;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 
+import java.util.Objects;
+
 public class RegisterNewVehiclePresenter implements RegisterNewVehicleInterface.RegisterPresenter{
 
-    private RegisterNewVehicleInterface.View view;
-    private RegisterNewVehicleModel model;
+    private final RegisterNewVehicleInterface.View view;
+    private final RegisterNewVehicleModel model;
 
     // Constructor for the presenter class
     public RegisterNewVehiclePresenter(RegisterNewVehicleInterface.View carView, RegisterNewVehicleModel carModel){
@@ -19,7 +21,7 @@ public class RegisterNewVehiclePresenter implements RegisterNewVehicleInterface.
         model.checkIfCarExists(enteredLicensePlate, new RegisterNewVehicleModel.OnCarDatabaseCheckListener() {
             @Override
             public void OnCarExistInDB(DataSnapshot dataSnapshot) {
-                String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                String userID = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
                 model.addCarForUserInDB(userID, dataSnapshot, new RegisterNewVehicleModel.OnDatabaseUpdateListener() {
                     @Override
                     public void onSuccess() {
