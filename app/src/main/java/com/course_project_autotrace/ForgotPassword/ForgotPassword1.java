85c650d8fed344a9b87348153a07c4cc1e58de86
ji24077 <80298064+Hanji11@users.noreply.github.com>
2023-08-07 22:02:15 -0400

    Refactored the ForgotPassword1 file code, following Model view presenter design and CA.

--- app/src/main/java/com/course_project_autotrace/ForgotPassword/ForgotPassword1.java
+++ app/src/main/java/com/course_project_autotrace/ForgotPassword/ForgotPassword1.java
@@ -1,78 +1,52 @@
 package com.course_project_autotrace.ForgotPassword;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+
 import com.course_project_autotrace.Login.LoginScreen;
 import com.course_project_autotrace.R;
-import com.google.firebase.auth.FirebaseAuth;
 
-public class ForgotPassword1 extends AppCompatActivity {
-    Button btnResetContinue;
-    EditText edtEmail;
-    //ProgressBar progressBar;
-    FirebaseAuth mAuth;
-    String strEmail;
-    ImageButton bckButton;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class ForgotPassword1 extends AppCompatActivity implements ForgotPassword1View{
+    private EditText edtEmail;
+    private Button btnResetCont;
+    private ImageButton bckBtn;
+    private ForgotPassword1Presenter FPpresenter;
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forgot_password1);
-        //declare
-        btnResetContinue = findViewById(R.id.btnResetPwdContinue);
-        edtEmail = findViewById(R.id.editTextEmail);
-        //progressbar = ..
-        mAuth = FirebaseAuth.getInstance();
-        bckButton = findViewById(R.id.BackBtn);
-
-        //listener.
-        btnResetContinue.setOnClickListener(view -> {
-            strEmail = edtEmail.getText().toString().trim();
-            if (!TextUtils.isEmpty(strEmail)) {
-                ResetPassword();
-            } else {
-                edtEmail.setError("Enter the Email");
+        setContentView((R.layout.activity_forgot_password1));
+        edtEmail=findViewById(R.id.editTextEmail);
+        bckBtn=findViewById(R.id.BackBtn);
+        btnResetCont = findViewById(R.id.btnResetPwdContinue);
+        FPpresenter = new ForgotPassword1Presenter(this);
+        btnResetCont.setOnClickListener(view -> {
+            String strEmail= edtEmail.getText().toString().trim();
+            if(!TextUtils.isEmpty(strEmail)){
+                FPpresenter.ResetPassword1(strEmail);
             }
-
+            else{
+                edtEmail.setError("Enter the email");
+            }
         });
-        bckButton.setOnClickListener(v-> {
-
-            //  code for Forgotpassword clicked
+        bckBtn.setOnClickListener(view -> {
             Intent intent = new Intent(ForgotPassword1.this, LoginScreen.class);
             startActivity(intent);
-
         });
-
-
     }
-
-    //can be refactored by design principle/pattern.
-    private void ResetPassword() {
-        btnResetContinue.setVisibility(View.INVISIBLE);
-        mAuth.sendPasswordResetEmail(strEmail)
-                .addOnSuccessListener(unused -> {
-                    // Reset email sent successfully. Handle your success case here if needed.
-                    Toast.makeText(ForgotPassword1.this,"Reset Password Link has been sent to your registered Email",Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(ForgotPassword1.this, LoginScreen.class);
-                    startActivity(intent);
-                    finish();
-                })
-                .addOnFailureListener(e -> {
-                    // Handle your failure case here if needed.
-                    Toast.makeText(ForgotPassword1.this, "Error:-"+ e.getMessage(), Toast.LENGTH_SHORT).show();
-
-
-                });
-        btnResetContinue.setVisibility(View.VISIBLE);
+    public void showPasswordResetSuccess(){
+        Toast.makeText(ForgotPassword1.this,"Reset Password Link has been sent to your registered Email",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(ForgotPassword1.this,LoginScreen.class);
+        startActivity(intent);
+        finish();
+    }
+    public void showPasswordResetFailed(String Error){
+        Toast.makeText(ForgotPassword1.this,"Error:"+Error,Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/course_project_autotrace/ForgotPassword/ForgotPassword1View.java
@@ -0,0 +1,6 @@
+package com.course_project_autotrace.ForgotPassword;
+
+public interface ForgotPassword1View {
+    void showPasswordResetSuccess();
+    void showPasswordResetFailed(String Error);
+}
