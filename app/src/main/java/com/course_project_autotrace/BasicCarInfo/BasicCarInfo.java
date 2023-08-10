17c8ce66b49cd4a5f340e83e9f6fa0bb65a19359
Haris Malik <112772076+harismalik-1@users.noreply.github.com>
2023-08-10 01:21:17 -0400

    Javadocs added for BasicCarInfo and BasicCarInfoPresenter

--- app/src/main/java/com/course_project_autotrace/BasicCarInfo/BasicCarInfo.java
+++ app/src/main/java/com/course_project_autotrace/BasicCarInfo/BasicCarInfo.java
@@ -8,31 +8,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-
+/**
+ * An activity class that displays basic car information to the user.
+ * It retrieves the car details, displays them, and provides a way
+ * to navigate back to the Login screen.
+ */
 public class BasicCarInfo extends AppCompatActivity implements BasicCarInfoView {
+
     private BasicCarInfoPresenter BasicPresenter;
     private TextView infoBoxTextView, carNameTextview, modelYearTextView;
 
+    /**
+     * Called when the activity is starting.
+     * Sets the content view and initializes UI components.
+     *
+     * @param savedInstanceState if the activity is being re-initialized after
+     * previously being shut down then this Bundle contains the data it most
+     * recently supplied in onSaveInstanceState(Bundle). Otherwise, it is null.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView((R.layout.activity_basic_car_info));
 
-        // link the textviews with the respective ids from the resource xml
+        // Link the TextViews with the respective IDs from the resource XML
         modelYearTextView = findViewById(R.id.ModelYear);
         carNameTextview = findViewById(R.id.CarName);
         infoBoxTextView = findViewById(R.id.InfoBox);
 
-        // Fetch the car info
+        // Fetch and display the car info
         FetchCarInfoUseCase fetchInfoUseCase = new FetchCarInfoUseCase();
         BasicPresenter = new BasicCarInfoPresenter(this, fetchInfoUseCase);
         BasicPresenter.loadCarInfo(getIntent());
 
-        // back button to navigate back to the LoginScreen
+        // Set up a back button to navigate back to the LoginScreen
         ImageButton BackBtn = findViewById(R.id.BackBtn);
         BackBtn.setOnClickListener(v -> BasicPresenter.handleBackClicked());
     }
 
+    /**
+     * Displays car details including model, name, insurance, and additional info.
+     *
+     * @param car the car entity containing the details to be displayed.
+     */
     @Override
     public void displayCarDetails(CarEntity.Car car) {
         modelYearTextView.setText(car.getModel());
@@ -42,10 +60,12 @@ public class BasicCarInfo extends AppCompatActivity implements BasicCarInfoView 
         infoBoxTextView.setText(InfoFormat);
     }
 
+    /**
+     * Navigates the user to the LoginScreen activity.
+     */
     @Override
     public void navigateToLoginScreen() {
         Intent newIntent = new Intent(BasicCarInfo.this, LoginScreen.class);
         startActivity(newIntent);
-
     }
 }
--- app/src/main/java/com/course_project_autotrace/BasicCarInfo/BasicCarInfoPresenter.java
+++ app/src/main/java/com/course_project_autotrace/BasicCarInfo/BasicCarInfoPresenter.java
@@ -2,25 +2,43 @@ package com.course_project_autotrace.BasicCarInfo;
 
 import android.content.Intent;
 
+/**
+ * Presenter class for the BasicCarInfo. It handles the business logic for
+ * fetching and displaying basic car information, as well as navigation actions.
+ */
 public class BasicCarInfoPresenter {
 
-    // variable for presenter class
+    // The view associated with this presenter.
     private final BasicCarInfoView carView;
+
+    // The use case for fetching car information.
     private final FetchCarInfoUseCase fetchInfoUseCase;
 
-    // Constructor for BasicCarInfoFectchPresenter
+    /**
+     * Constructs a new BasicCarInfoPresenter with the given view and use case.
+     *
+     * @param carView the view to be updated by this presenter.
+     * @param fetchInfoUseCase the use case for fetching car information.
+     */
     public BasicCarInfoPresenter(BasicCarInfoView carView, FetchCarInfoUseCase fetchInfoUseCase){
         this.carView = carView;
         this.fetchInfoUseCase = fetchInfoUseCase;
     }
 
-    // Load the car info method
+    /**
+     * Loads and displays car information using data from the provided intent.
+     *
+     * @param intent the intent containing the car data to be loaded.
+     */
     public void loadCarInfo(Intent intent){
         CarEntity.Car car1 = fetchInfoUseCase.fromIntent(intent);
         carView.displayCarDetails(car1);
     }
 
-    // method to navigate back to the LoginScreen
+    /**
+     * Handles a back navigation action, triggering the view to navigate back to
+     * the LoginScreen.
+     */
     public void handleBackClicked(){
         carView.navigateToLoginScreen();
     }
