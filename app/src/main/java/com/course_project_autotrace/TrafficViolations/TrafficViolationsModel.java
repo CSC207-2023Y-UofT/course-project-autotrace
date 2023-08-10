6f8abd5281344dbd0f8284f096feffd65e2de3d2
Haris Malik <112772076+harismalik-1@users.noreply.github.com>
2023-08-10 01:39:15 -0400

    Javadocs added for TrafficViolationModel

--- app/src/main/java/com/course_project_autotrace/TrafficViolations/TrafficViolationsModel.java
+++ app/src/main/java/com/course_project_autotrace/TrafficViolations/TrafficViolationsModel.java
@@ -12,15 +12,36 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
 
+/**
+ * Represents the model for fetching traffic violations data.
+ * It interacts with Firebase to retrieve traffic violation data associated with a specific driver license.
+ */
 public class TrafficViolationsModel {
 
+    /**
+     * Callback interface to handle data load events.
+     */
     interface OnDataLoaded {
+        /**
+         * Called when traffic violations data is successfully loaded.
+         *
+         * @param violations The snapshot containing the traffic violation data.
+         */
         void onDataLoaded(DataSnapshot violations);
+
+        /**
+         * Called when there is an error while fetching data.
+         *
+         * @param error Error message.
+         */
         void onError(String error);
     }
 
     private final DatabaseReference referenceToViolations;
 
+    /**
+     * Initializes the model and sets up Firebase references.
+     */
     public TrafficViolationsModel() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         assert user != null;
@@ -29,12 +50,18 @@ public class TrafficViolationsModel {
         referenceToViolations = FirebaseDatabase.getInstance().getReference().child("TrafficViolations");
     }
 
+    /**
+     * Fetches traffic violation data for the provided driver license from Firebase.
+     *
+     * @param driverLicense The driver license to fetch violations for.
+     * @param callback      Callback to handle data loaded events.
+     */
     public void fetchViolationsData(String driverLicense, OnDataLoaded callback) {
         referenceToViolations.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 DataSnapshot violations = null;
-                for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     if (Objects.equals(snapshot.getKey(), driverLicense)) {
                         violations = snapshot;
                     }
